393c5903de82332fb0989adf2d2615e2ed52446f
Megan Coyle <meg@mcoyle.com>
2016-09-04 18:14:10 -0400

    Add text for winner when game is complete

--- public/js/main.controller.js
+++ public/js/main.controller.js
@@ -12,6 +12,7 @@ angular
     // var player2 = 'O';
     var plays = 1;
     var turn = 0;
+    vm.winner = "no one"
     vm.endGame = false;
 
   // set up initial board values
@@ -71,49 +72,49 @@ angular
       // 00, 01, 02 (top row)
       if (vm.board[0][0].value && vm.board[0][0].value === vm.board[0][1].value &&
         vm.board[0][1].value === vm.board[0][2].value) {
-          var winner = vm.board[0][0].value;
+          vm.winner = vm.board[0][0].value;
           // console.log(winner, 'wins (top row)');
           vm.endGame = true;
       // 10, 11, 12 (middle row)
       } else if (vm.board[1][0].value && vm.board[1][0].value === vm.board[1][1].value &&
         vm.board[1][1].value === vm.board[1][2].value) {
-          var winner = vm.board[1][0].value;
+          vm.winner = vm.board[1][0].value;
           // console.log(winner, 'wins (middle row)');
           vm.endGame = true;
       // 20, 21, 22 (bottom row)
       } else if (vm.board[2][0].value && vm.board[2][0].value === vm.board[2][1].value &&
         vm.board[2][1].value === vm.board[2][2].value) {
-          var winner = vm.board[2][0].value;
+          vm.winner = vm.board[2][0].value;
           // console.log(winner, 'wins (bottom row)');
           vm.endGame = true;
       // 00, 10, 20 (left column)
       } else if (vm.board[0][0].value && vm.board[0][0].value === vm.board[1][0].value &&
         vm.board[1][0].value === vm.board[2][0].value) {
-          var winner = vm.board[0][0].value;
+          vm.winner = vm.board[0][0].value;
           // console.log(winner, 'wins (first column)');
           vm.endGame = true;
       // 01, 11, 21 (middle column)
       } else if (vm.board[0][1].value && vm.board[0][1].value === vm.board[1][1].value &&
         vm.board[1][1].value === vm.board[2][1].value) {
-          var winner = vm.board[0][1].value;
+          vm.winner = vm.board[0][1].value;
           // console.log(winner, 'wins (middle column)');
           vm.endGame = true;
       // 02, 12, 22 (right column)
       } else if (vm.board[0][2].value && vm.board[0][2].value === vm.board[1][2].value &&
           vm.board[1][2].value === vm.board[2][2].value) {
-            var winner = vm.board[0][2].value;
+            vm.winner = vm.board[0][2].value;
             // console.log(winner, 'wins (last column)');
             vm.endGame = true;
       // 00, 11, 22 ( top-left, bottom-right diagonal )
       } else if (vm.board[0][0].value && vm.board[0][0].value === vm.board[1][1].value &&
             vm.board[1][1].value === vm.board[2][2].value) {
-              var winner = vm.board[0][0].value;
+              vm.winner = vm.board[0][0].value;
               // console.log(winner, 'wins (diagonal 1)');
               vm.endGame = true;
       // 02, 11, 20 ( bottom-left, top-right diagonal)
       } else if (vm.board[0][2].value && vm.board[0][2].value === vm.board[1][1].value &&
           vm.board[1][1].value === vm.board[2][0].value) {
-            var winner = vm.board[0][2].value;
+            vm.winner = vm.board[0][2].value;
             // console.log(winner, 'wins (diagonal 1)');
             vm.endGame = true;
       } else if (plays == 9) {
